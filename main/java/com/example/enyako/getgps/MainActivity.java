d168b20125cf0d2a7f58387387ae1ad02fadd9d8
EnyaKo <enyako530@gmail.com>
2017-10-19 00:02:27 +0800

    sec commit
    wrong file uploaded

---
+++ main/java/com/example/enyako/getgps/MainActivity.java
@@ -0,0 +1,112 @@
+package com.example.enyako.getgps;
+
+//import android.Manifest;
+//import android.content.Intent;
+//import android.content.pm.PackageManager;
+import android.location.Location;
+//import android.location.LocationListener;
+//import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+//import android.provider.Settings;
+//import android.support.annotation.NonNull;
+//import android.support.annotation.Nullable;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements ConnectionCallbacks, OnConnectionFailedListener {
+    protected static final String TAG = "MainActivity";
+
+    protected GoogleApiClient mGoogleApiClient;
+    protected Location mLastLocation;
+
+    protected String mLatitudeLabel;
+    protected String mLongitudeLabel;
+    protected TextView mLatitudeText;
+    protected TextView mLongitudeText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLatitudeLabel = "緯度";
+        mLongitudeLabel = "經度";
+        mLatitudeText = (TextView) findViewById((R.id.latitude_text));
+        mLongitudeText = (TextView) findViewById((R.id.longitude_text));
+
+        buildGoogleApiClient();
+    }
+
+    protected synchronized void buildGoogleApiClient()
+    {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        if (mGoogleApiClient.isConnected())
+        {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+
+    // These below will run after GoogleApiClient connects Google Play Service
+    @Override
+    public void onConnected(Bundle connectionHint)
+    {
+        // ignore that this line will be underlined in red in IDE
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null)
+        {
+            mLatitudeText.setText(String.format("%s: %f", mLatitudeLabel, mLastLocation.getLatitude()));
+            mLongitudeText.setText(String.format("%s: %f", mLongitudeLabel, mLastLocation.getLongitude()));
+        }
+        else
+        {
+            Toast.makeText(this, "偵測不到定位，請確認定位功能已開啟。", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result)
+    {
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int cause)
+    {
+        Log.i(TAG, "Connection suspended");
+        mGoogleApiClient.connect();
+    }
+}
+
